4fb2788c02e3b8d7ddfd505d6d3c4cdc2ef206f9
Andrew Tann <andrew.tann1@det.nsw.edu.au>
2025-11-08 23:52:17 +1100

    fix: Correct tailwind content path to scan all project files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./{components,contexts,services,types,constants}/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
